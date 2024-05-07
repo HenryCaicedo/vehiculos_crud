f59bac5dcebeeca612a6d403c948dace14e1a46a
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2024-05-06 22:55:13 -0500

    Icons added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+// import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -22,7 +23,8 @@ import { VehicleCardComponent } from './components/vehicle-card/vehicle-card.com
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    // MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
